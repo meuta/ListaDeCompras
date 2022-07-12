a3972f358d50e5482bd40a009b4f0fdb53c5b52c
meuta <efanya@gmail.com>
2022-07-12 14:47:32 +0100

    Why ViewHolder & make Adapter

---
+++ app/src/main/java/com/example/listadecompras/presentation/ShopListAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.listadecompras.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.listadecompras.R
+import com.example.listadecompras.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+        holder.view.setOnClickListener {
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+}
