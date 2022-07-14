e3b0042fe344cd3859add04d15656a98f1988140
meuta <efanya@gmail.com>
2022-07-14 14:59:33 +0100

    Layers for the ShopItemActivity

---
+++ app/src/main/java/com/example/listadecompras/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.listadecompras.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.listadecompras.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
